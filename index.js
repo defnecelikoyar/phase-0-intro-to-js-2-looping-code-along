f9a53f309702ac368af7230b702c591c21b35863
Defne Celikoyar <defne.celikoyar@gmail.com>
2021-11-26 13:18:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Ayse", "Defne", "Yasemin", "Ipek"];
+const specialDay = "birthday";
+const messageArray = [];
+
+function writeCards(names, specialDay) {
+    for (let i=0; i < names.length; i++) {
+        debugger;
+        messageArray.push(`Thank you, ${names[i]}, for the wonderful ${specialDay} gift!`);
+    }
+    return messageArray;
+}; 
+
+function countDown(numba = 10) {
+    while (numba >= 0) {
+    console.log(numba);
+    numba--;
+};
+};
+
